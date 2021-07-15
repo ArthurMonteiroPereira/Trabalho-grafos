5b3b026e2b9df413efd9db30eaf0a152851ced49
ArthurUFJF <arthurmpereira2010@gmail.com>
2021-07-15 03:41:05 -0300

    main fix

--- main.cpp
+++ main.cpp
@@ -6,16 +6,26 @@ using namespace std;
 
 int main()
 {
-    int arnaldo=2;
+    FILE *arquivo = fopen("teste.txt", "r");
+    char linha[100];
+    if(arquivo != NULL){
+
+    while (!feof(arquivo))
+    {
+        string resultado(fgets(linha,100,arquivo));
+
+        if(!resultado.empty()){
+            string lin(linha);
+            cout << lin << endl;
+        }
+    }
     grafo teste(10);
-    cout << "Hello world!" << endl;
-    teste.adicionaAresta(0,1);
     teste.adicionaAresta(4,3);
     teste.adicionaAresta(2,3);
     teste.imprime();
     teste.removeAresta(3,4);
     teste.imprime();
-
-
+    }
+    else cout<<"Problemas ao abrir arquivo!" << endl;
     return 0;
 }
